7ca8db5525039153b6936f408c9e4212224e6137
ciecclm <clm@imnu.edu.cn>
2018-10-13 08:49:17 +0800

    Initial Commit

---
+++ test1013/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test1013
+//
+//  Created by 朝力萌 on 2018/10/13.
+//  Copyright © 2018年 朝力萌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
