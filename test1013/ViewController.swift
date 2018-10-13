ccbb75a9d40714d19fc351d46a36d4ed95f7caad
ciecclm <clm@imnu.edu.cn>
2018-10-13 09:51:27 +0800

    x加y等于Z
    计算完成

--- test1013/ViewController.swift
+++ test1013/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var clmX: UITextField!
+    @IBOutlet var clmY: UITextField!
+    @IBOutlet var clmZ: UITextField!
+    
+    
+    @IBAction func caculator(_ sender: Any) {
+        var x = 0
+        x = Int(clmX.text!)!
+        var y = 0
+        y = Int(clmY.text!)!
+        var z:Int=0
+            z = x + y
+        clmZ.text = "\(z)"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        clmX.text = "0"
+        clmY.text = "0"
+        clmZ.text = "0"
     }
 
 
